ee2946da6798f255c224c24471c8367a3a13d408
Misha Kharitonov <45524887+mishakh@users.noreply.github.com>
2019-07-31 12:12:27 -0400

    Add files via upload

---
+++ processor6.py
@@ -0,0 +1,69 @@
+#Assignment 5 ARM Processor
+import processor_c as arm       #import classes/functions
+
+####INITIAL
+#load Instruction Memory
+f = open("assignment5.txt", 'r')
+instrucMem = f.readlines()
+PC=0
+reg=arm.Registers()     #create register object with Registers and Data Memory
+reg.dataMem[0]=10      
+reg.dataMem[1]=13       #load in data memory array values
+
+####RUN TIME
+while(PC<len(instrucMem)):   
+        NPC=PC+1        #calculate New Program Counter
+        inReg=arm.InstructionReg(instrucMem[PC])   #Instruction Fetch
+        
+#Instruction Deconstruction Occurs in IR constructor function
+
+        controlU=arm.Control()  #creates control unit for this instruction (with all flags set low)
+        controlU.insRead(inReg.opcode)  #Set control bit values
+
+        reg.readRegs(inReg.Rn, inReg.Rm) #read in values from Rn and Rm registers
+        if(controlU.branch==1):
+                ALU=arm.ALU(reg.X[inReg.Rd],inReg.Imm,controlU.ALUop1,controlU.ALUop2)
+                                        #creates ALU operation for B type instructions
+        elif(controlU.ALUSrc==1):
+                ALU=arm.ALU(reg.readData1, inReg.Imm, controlU.ALUop1, controlU.ALUop2)
+                                        #creates ALU operation for I type and D type instructions
+        elif(controlU.ALUSrc==0):
+                ALU=arm.ALU(reg.readData1, reg.readData2, controlU.ALUop1, controlU.ALUop2)
+                                        #creates ALU operation for R type instructions
+        ALU.ALUcontrol(ALU.ALU_op,inReg.opcode)
+                                        #read in ALU op and set the appropriate ALU operation
+        ALU.exec(ALU.ALU_c)     #perform ALU operation, place result in ALU.output
+
+        if((controlU.memToReg==0) and (controlU.branch==0)):
+                reg.regWrite(inReg.Rd,ALU.output)       #write back for R type instructions
+                PC=NPC
+        elif(controlU.memToReg==1):
+                if(controlU.memRead==1): #LOAD
+                        reg.regWrite(inReg.Rd, reg.dataMem[ALU.output])  #load data memory value to Rd                      
+                        print('Data Memory:\n',reg.dataMem)
+                elif(controlU.memWrite==1): #STORE
+                        reg.dataMem[ALU.output]=reg.X[inReg.Rd] #load Rd value to calculated Data Memory Address
+                        print('Data Memory:\n',reg.dataMem)
+
+                PC=NPC #go to next sequential instrution
+                
+        elif(controlU.branch==1):  #Check for branch opcode
+                if(controlU.uncond==1):  # B instruction
+                        PC= PC+inReg.Imm  #PC <- PC+addr
+                        print("PC: "+str(PC))
+                        print('-----------------------\n')
+                        continue
+                elif(ALU.zero==1):  #if R[Rd]==0
+                        PC = PC+inReg.Imm  #PC <- PC+addr
+                        print('Registers:\n',(reg.X))
+                        print("PC: "+str(PC))
+                        print('-----------------------\n')
+                        continue
+                else:
+                        print('Registers:\n',(reg.X))
+                        print("PC: "+str(PC)) #if CBZ result is false, continue sequentially
+                        PC=NPC
+        print('Registers:\n',(reg.X))
+        print("PC: "+str(PC))
+        print('-----------------------\n')
+print('END OF FILE')
---
+++ processor6_c.py
@@ -0,0 +1,146 @@
+#contains all classes used in processor.py, imported into processor.py as arm
+class Registers:
+	def __init__(self):
+		self.X=[0]*32 		#Register Array X0-X31
+		self.dataMem=[0]*32 	#Data Memory Array w/ 32 blocks
+		readData1 = 0 		#
+		readData2 = 0 		#loaded register values go here
+	def regWrite(self, wReg, wData):
+		self.X[wReg]=wData 	#write data to register
+		print(wData,' in X',wReg,'\t--Register Write')
+
+	def readRegs(self, rReg1, rReg2): # Read from 2 registers
+		self.readData1=self.X[rReg1]
+		self.readData2=self.X[rReg2]
+		#print(self.readData1, self.readData2)
+
+	def readReg(self, rReg): # Read from single register
+		self.readData1=self.X[rReg]
+
+
+class Control:
+
+	def __init__(self):
+		self.reg2Loc = 0 	#
+		self.branch = 0 	#
+		self.memRead = 0 	#
+		self.memToReg = 0 	#
+		self.ALUop1 = 0 	# instantiate control bits to 0
+		self.ALUop2 = 0 	#
+		self.memWrite = 0 	#
+		self.ALUSrc = 0 	#
+		self.regWrite = 0 	#
+		self.uncond = 0 	#
+
+
+	def insRead(self, opcode):
+		#reads opcode and sets control bit values
+		if(opcode==('ADD') or opcode==('SUB') or opcode==('AND') or opcode==('ORR')):
+			self.regWrite = 1
+			self.ALUop1 = 1
+
+		elif(opcode==('LDUR') or opcode==('STUR')):
+			self.ALUSrc = 1
+			self.memToReg = 1
+			self.regWrite = 1
+			if opcode==('LDUR'):
+				self.memRead = 1
+			elif opcode==('STUR'):
+				self.memWrite = 1
+
+		elif(opcode==('ADDI') or opcode==('SUBI')):
+			self.regWrite = 1
+			self.ALUop1 = 1
+			self.ALUSrc = 1
+
+		elif(opcode==('CBZ') or opcode==('B')):
+			self.ALUop2 = 1
+			self.branch = 1
+			self.ALUSrc = 1
+			if(opcode=='B'):
+				self.uncond = 1
+
+class InstructionReg:
+	opcode=0
+	Rd=0
+	Rn=0
+	Rm=0
+	Imm=0
+	def __init__(self, instruc):
+		instruc=instruc.replace(",","")     ##format instruction
+		instruc=instruc.replace("#","")     ##for code to read
+		instruc=instruc.replace("XZR","0")  ##
+		instruc=instruc.replace("X","")     ##
+		instruc=instruc.replace("[","")     ##
+		instruc=instruc.replace("]","")     ##
+		instruc=instruc.split()             #seperate instruction into addressable entities
+		self.opcode=instruc[0]
+		if(self.opcode=='ADD' or self.opcode=='SUB' or self.opcode=='AND' or self.opcode=='ORR'):
+			self.Rd=int(instruc[1])
+			self.Rn=int(instruc[2])     # R-Format
+			self.Rm=int(instruc[3])
+			print(self.opcode,self.Rd,self.Rn,self.Rm,'\t--Instruction Register Read')
+		elif (self.opcode=='LDUR' or self.opcode=='STUR'):
+			self.Rd=int(instruc[1])
+			self.Rn=int(instruc[2])     # D-Format
+			self.Imm= int(instruc[3])
+			print(self.opcode,self.Rd,self.Rn,self.Imm,'\t--Instruction Register Read')
+		elif(self.opcode=='ADDI' or self.opcode=='SUBI'):
+			self.Rd=int(instruc[1])
+			self.Rn=int(instruc[2])     # I-Format
+			self.Imm=int(instruc[3])
+			print(self.opcode,self.Rd,self.Rn,self.Imm,'\t--Instruction Register Read')
+		elif(self.opcode=='B' or self.opcode=='CBZ'):
+			self.Imm=int(instruc[1])
+			if(self.opcode=='CBZ'):     # CBZ-Format
+				self.Rd=int(instruc[1])
+				self.Imm=int(instruc[2])
+				print(self.opcode,self.Rd,self.Imm,'\t--Instruction Register Read')
+			elif(self.opcode=='B'):
+				self.Imm=int(instruc[1])
+				print(self.opcode,self.Imm,'\t\t--Instruction Register Read')
+
+class ALU:
+
+	def __init__(self,in1,in2,ALU_op1,ALU_op2):
+		self.in1 = in1
+		self.in2 = in2
+		self.ALU_op=str(ALU_op1)+str(ALU_op2) 	#ALU opcode to be used by ALU control 
+		self.ALU_c = 0
+		self.output = 0
+		self.zero = 0
+
+	def ALUcontrol(self, ALUop, opcode):
+		if(ALUop == '00'):
+			#LDUR AND STUR operations
+			self.ALU_c = 0 #add
+		if (ALUop == '01'):
+			#B
+			if(opcode=='B'):
+				self.ALU_c = 2 #pass b
+			#CBZ
+			elif(opcode=='CBZ'):
+				self.ALU_c = 3 #compare to zero
+		if (ALUop == '10'):
+			if((opcode=='ADD') or (opcode=='ADDI')):
+				self.ALU_c = 0 #add
+			elif((opcode=='SUB') or (opcode=='SUBI')):
+				self.ALU_c = 1 #sub
+			elif(opcode=='AND'):
+				self.ALU_c = 4 #AND
+			elif(opcode=='ORR'):
+				self.ALU_c = 5 #ORR
+
+	# This will execute whatever operation called at the method
+	def exec(self, control):
+		control=self.ALU_c
+
+		if(control==0):
+			self.output=self.in1+self.in2 #addition
+		if(control==1):
+			self.output=self.in1-self.in2 #subtraction
+		if(control==2):
+			self.output=self.in1 #pass value
+		if(control==3):
+			if(self.in1==0): #check for zero, set ALU.zero to 1 if true
+				self.zero=1
