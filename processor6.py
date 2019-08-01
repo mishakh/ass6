4a7f1895dd46385ea3fc0537d78fd201ac119f44
smithdepazd <44277887+smithdepazd@users.noreply.github.com>
2019-08-01 14:29:44 -0400

    Add files via upload

--- processor6.py
+++ processor6.py
@@ -1,84 +1,36 @@
 #Assignment 5 ARM Processor
-import processor6_c as arm       #import classes/functions
+# import processor6_c as arm       #import classes/functions
+from steps import *
 
-####INITIAL
-#load Instruction Memory
-f = open("assignment5.txt", 'r')
-instrucMem = f.readlines()
-PC=0
-reg=arm.Registers()     #create register object with Registers and Data Memory
-reg.dataMem[0]=10      
-reg.dataMem[1]=13       #load in data memory array values
 
-####RUN TIME
-while(PC<len(instrucMem)):
-        # FETCH
-        NPC=PC+1        #calculate New Program Counter
-        inReg=arm.InstructionReg(instrucMem[PC])   #Instruction Fetch
-        # END OF FETCH
-# Instruction Deconstruction Occurs in IR constructor function
+#instatiate pipeline registers
+decode_in = arm.pipeReg()
+execute_in = arm.pipeReg()
+writeback_in = arm.pipeReg()
+memory_in = arm.pipeReg()
 
-        #DECODE
-        controlU=arm.Control()  # creates control unit for this instruction (with all flags set low)
-        controlU.insRead(inReg.opcode)  # Set control bit values
-
-        reg.readRegs(inReg.Rn, inReg.Rm) # read in values from Rn and Rm registers
-        # END OF DECODE
-
-        # EXECUTE
-        if(controlU.branch==1):
-                ALU=arm.ALU(reg.X[inReg.Rd],inReg.Imm,controlU.ALUop1,controlU.ALUop2)
-                                        # creates ALU operation for B type instructions
-        elif(controlU.ALUSrc==1):
-                ALU=arm.ALU(reg.readData1, inReg.Imm, controlU.ALUop1, controlU.ALUop2)
-                                        # creates ALU operation for I type and D type instructions
-        elif(controlU.ALUSrc==0):
-                ALU=arm.ALU(reg.readData1, reg.readData2, controlU.ALUop1, controlU.ALUop2)
-
-                                      # creates ALU operation for R type instructions
-        ALU.ALUcontrol(ALU.ALU_op,inReg.opcode)
-                                        # read in ALU op and set the appropriate ALU operation
-        ALU.exec(ALU.ALU_c)     #perform ALU operation, place result in ALU.output
-
-        # END EXECUTE
-
-        # WRITEBACK
-
-        if((controlU.memToReg==0) and (controlU.branch==0)):
-                reg.regWrite(inReg.Rd,ALU.output)       #write back for R type instructions
-                PC=NPC
-        # END OF WRITE BACK
-
-        # MEMORY
-        if(controlU.memToReg==1):
-                if(controlU.memRead==1): #LOAD
-                        reg.regWrite(inReg.Rd, reg.dataMem[ALU.output])  #load data memory value to Rd                      
-                        print('Data Memory:\n',reg.dataMem)
-                elif(controlU.memWrite==1): #STORE
-                        reg.dataMem[ALU.output]=reg.X[inReg.Rd] #load Rd value to calculated Data Memory Address
-                        print('Data Memory:\n',reg.dataMem)
-
-                PC=NPC #go to next sequential instrution
-
-        elif(controlU.branch==1):  #Check for branch opcode
-                if(controlU.uncond==1):  # B instruction
-                        PC= PC+inReg.Imm  #PC <- PC+addr
-                        print("PC: "+str(PC))
-                        print('-----------------------\n')
-                        continue
-                elif(ALU.zero==1):  #if R[Rd]==0
-                        PC = PC+inReg.Imm  #PC <- PC+addr
-                        print('Registers:\n',(reg.X))
-                        print("PC: "+str(PC))
-                        print('-----------------------\n')
-                        continue
-                else:
-                        print('Registers:\n',(reg.X))
-                        print("PC: "+str(PC)) #if CBZ result is false, continue sequentially
-                        PC=NPC
-
-        # END MEMORY
+# ###RUN TIME
+while PC<len(instrucMem):
         
+        fetch_out = fetch()
+        decode_out = decode(decode_in)
+        execute_out = execute(execute_in)
+        memory_out = memory(memory_in)
+        writeback_out = writeback(writeback_in)
+
+        # this block of code ensures that the processor doesn't repeat the same instruction
+        if writeback_out.PC > PC:
+                PC = writeback_out.PC
+        else:   #
+                PC += 1
+
+        decode_in = fetch_out
+        execute_in = decode_out
+        memory_in = execute_out
+        writeback_in = memory_out
+
+
+
         print('Registers:\n',(reg.X))
         print("PC: "+str(PC))
         print('-----------------------\n')
